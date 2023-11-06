63c8616f8daab0a2b56029227cba49c578d76048
Raymond Ha <Haro@mail.uc.edu>
2023-11-06 18:04:25 -0500

    Final Version

--- src/TagExtractorRunner.java
+++ src/TagExtractorRunner.java
@@ -1,5 +1,7 @@
-public class Main {
+public class TagExtractorRunner {
     public static void main(String[] args) {
+
+        TagExtractorFrame extractorFrame = new TagExtractorFrame();
         System.out.println("Hello world!");
     }
 }
